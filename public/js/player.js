74da78cf214a7570febe72dddde5a6703060eaa1
Nick Wills <37917347+KingOfCaves@users.noreply.github.com>
2019-11-20 23:37:42 -0500

    something's wrong with mixxx sending the metadata. comes up as broken? adding some logs to see what it's sending.

--- public/js/player.js
+++ public/js/player.js
@@ -16,11 +16,12 @@
 		volumeStored = +localStorage.getItem('volume');
 	} else {
 		volumeStored = +localStorage.getItem('volume');
-		console.log(+localStorage.getItem('volume'));
+		// console.log(+localStorage.getItem('volume'));
 	}
 
 	// FUNCTIONS
 	function infoUpdate(info) {
+		console.log(info);
 		const [artist, title] = info.split('//').map((i) => i.trim());
 
 		$infoArtist.textContent = artist;
