f9ef1814d3222e2519f764f3ea7140759b68a868
Nick Wills <37917347+KingOfCaves@users.noreply.github.com>
2021-05-05 18:25:39 -0400

    don't need useRef in this component

--- root/frontend/src/components/CoverartWindow.js
+++ root/frontend/src/components/CoverartWindow.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import WindowBorder from './WindowBorder';
 import Loader from './Loader';
 
