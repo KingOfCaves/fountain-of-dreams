7551ac3e19ee9511c0a0497a62c938314d42d35b
Nick Wills <37917347+KingOfCaves@users.noreply.github.com>
2021-09-20 13:59:28 -0400

    adding reference images to repo; getting rid of some ol bullshit; fixing a bunch of css/styling related bugs, fixing general terrible media queries

--- root/backend/scripts/scraper.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-const jsdom = require('jsdom');
-
-request('https://jpop80ss2.blogspot.com', (err, res, body) => {
-	const dom = new jsdom.JSDOM(body);
-	const doc = dom.window.document;
-	console.log(doc.querySelector('#sidebar-right-1h'));
-});
