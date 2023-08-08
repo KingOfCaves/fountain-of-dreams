8e5309a836d66bcf00c648886da8d2682096dec9
Nick Wills <nickolas.a.wills@gmail.com>
2023-08-08 13:22:07 -0400

    woops

--- root/frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './styles/main.scss';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
